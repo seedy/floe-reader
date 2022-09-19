b3c2b9d2f8bd6de6107f727f5aa4fd5bdf8106b7
Seedy <seedy.dupuis@gmail.com>
2022-09-19 22:49:19 +0200

    chore(theme): utils

--- stitches.config.ts
+++ stitches.config.ts
@@ -20,9 +20,20 @@ export const { styled, getCssText } = createStitches({
     bp1: '(min-width: 480px)'
   },
   utils: {
+    m: (value: Stitches.PropertyValue<'margin'>) => ({margin: value}),
+    mt: (value: Stitches.PropertyValue<'marginTop'>) => ({marginTop: value}),
+    mb: (value: Stitches.PropertyValue<'marginBottom'>) => ({marginBottom: value}),
     mx: (value: Stitches.PropertyValue<'marginLeft'| 'marginRight'>) => ({marginLeft: value, marginRight: value}),
     my: (value: Stitches.PropertyValue<'marginTop' | 'marginBottom'>) => ({marginTop: value, marginBottom: value}),
+    p: (value: Stitches.PropertyValue<'padding'>) => ({padding: value}),
+    pt: (value: Stitches.PropertyValue<'paddingTop'>) => ({paddingTop: value}),
+    pb: (value: Stitches.PropertyValue<'paddingBottom'>) => ({paddingBottom: value}),
     px: (value: Stitches.PropertyValue<'paddingLeft'| 'paddingRight'>) => ({paddingLeft: value, paddingRight: value}),
     py: (value: Stitches.PropertyValue<'paddingTop' | 'paddingBottom'>) => ({paddingTop: value, paddingBottom: value}),
+    size: (value: Stitches.PropertyValue<'width' | 'height'>) => ({
+      width: value,
+      height: value,
+    }),
+    br: (value: Stitches.PropertyValue<'borderRadius'>) => ({borderRadius: value})
   }
 });
