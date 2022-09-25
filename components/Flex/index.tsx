96a99db0125e5f09aabf3493bbf8b71ef93d0d29
Seedy <seedy.dupuis@gmail.com>
2022-09-25 23:04:54 +0200

    feat(Flex): flex component

---
+++ components/Flex/index.tsx
@@ -0,0 +1,50 @@
+import { styled } from "stitches.config";
+
+const Flex = styled("div", {
+  display: "flex",
+  variants: {
+    direction: {
+      row: {
+        flexDirection: "row",
+      },
+      column: {
+        flexDirection: "column",
+      },
+    },
+    align: {
+      center: {
+        alignItems: "center",
+      },
+      start: {
+        alignItems: "flex-start",
+      },
+      end: {
+        alignItems: "flex-end",
+      },
+      spaceBetween: {
+        alignItems: "space-between",
+      },
+    },
+    justify: {
+      center: {
+        justifyContent: "center",
+      },
+      start: {
+        justifyContent: "flex-start",
+      },
+      end: {
+        justifyContent: "flex-end",
+      },
+      spaceBetween: {
+        justifyContent: "space-between",
+      },
+    },
+    grow: {
+      true: {
+        flexGrow: 1,
+      },
+    },
+  },
+});
+
+export default Flex;
