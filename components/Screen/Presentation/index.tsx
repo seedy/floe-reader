8ccc2f69b76820af6349b14c012c2cbd2991273e
Seedy <seedy.dupuis@gmail.com>
2023-01-22 19:22:30 +0100

    feat(Component): screen and presentation (#30)

---
+++ components/Screen/Presentation/index.tsx
@@ -0,0 +1,66 @@
+import Screen from "components/Screen";
+import H1 from "components/Typography/H1";
+import H2 from "components/Typography/H2";
+import styles from "components/Screen/Presentation/Presentation.module.css";
+import Avatar, { AvatarProps, SIZE } from "components/Avatar";
+import Flex from "components/Flex";
+import floe from "public/floe.jpg";
+import P from "components/Typography/P";
+
+interface ScreenPresentationProps {
+  avatarProps?: Partial<AvatarProps>;
+}
+
+const ScreenPresentation = ({
+  avatarProps,
+  ...props
+}: ScreenPresentationProps) => (
+  <Flex align="center" justify="center">
+    <Screen
+      className={styles.content}
+      style={{ maxWidth: `calc(100vw - 2 * ${SIZE}px)` }}
+      {...props}
+    >
+      <Flex justify="center">
+        <H1>Présentation</H1>
+      </Flex>
+      <Flex direction="column" align="start">
+        <H2>Rédaction Web & SEO</H2>
+        <P
+          align="start"
+          className={styles.relativeBlock}
+          style={{ minHeight: SIZE }}
+        >
+          Lorem
+          <Avatar
+            className={styles.floatingAvatar}
+            style={{ left: `calc(${-SIZE}px - 1rem)` }}
+            src={floe}
+            alt="Floé"
+            {...avatarProps}
+          />
+        </P>
+      </Flex>
+      <Flex grow />
+      <Flex direction="column" align="end">
+        <H2>Correction</H2>
+        <P
+          align="end"
+          className={styles.relativeBlock}
+          style={{ minHeight: SIZE }}
+        >
+          Ipsum
+          <Avatar
+            className={styles.floatingAvatar}
+            style={{ right: `calc(${-SIZE}px - 1rem)` }}
+            src={floe}
+            alt="Floé"
+            {...avatarProps}
+          />
+        </P>
+      </Flex>
+    </Screen>
+  </Flex>
+);
+
+export default ScreenPresentation;
---
+++ components/Typography/P/P.stories.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { LOREM } from "constants/lorem";
+import P from "components/Typography/P";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Components/Typography/P",
+  component: P,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+} as ComponentMeta<typeof P>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof P> = (args) => <P {...args} />;
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+  children: LOREM.slice(0, 60),
+};
+
+export const AlignStart = Template.bind({});
+AlignStart.args = {
+  children: LOREM.slice(0, 60),
+  align: "start",
+};
+
+export const AlignEnd = Template.bind({});
+AlignEnd.args = {
+  children: LOREM.slice(0, 60),
+  align: "end",
+};
