5a39e119b692815110bea53d7c6337c0a064fd70
Seedy <seedy.dupuis@gmail.com>
2023-02-08 21:47:16 +0100

    38 component toast (#39)

---
+++ components/Toast/Provider/Provider.stories.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import ToastProvider from ".";
+import Button from "components/Button";
+import Toast from "components/Toast";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Components/ToastProvider",
+  component: ToastProvider,
+  subcomponents: { Toast },
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+} as ComponentMeta<typeof ToastProvider>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof ToastProvider> = (args) => (
+  <ToastProvider {...args} />
+);
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+  children: (
+    <div>
+      <input type="text" name="email" />
+      <Button type="submit">Prendre contact</Button>
+    </div>
+  ),
+};
