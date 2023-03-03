5885aeac623812000af80597c370464f114d6e32
Seedy <seedy.dupuis@gmail.com>
2023-03-03 14:46:35 +0100

    feat(ButtonLink): link variant of button

---
+++ components/Button/Link/Link.stories.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { StoryFn, Meta } from "@storybook/react";
+
+import Link from ".";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Components/Button/Link",
+  component: Link,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+} as Meta<typeof Link>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: StoryFn<typeof Link> = (args) => <Link {...args} />;
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+  children: "Ouvrir",
+};
+
+export const Contained = Template.bind({});
+
+Contained.args = {
+  children: "Ouvrir",
+  variant: "contained",
+};
+
+export const Text = Template.bind({});
+
+Text.args = {
+  children: "Ouvrir",
+  variant: "text",
+};
