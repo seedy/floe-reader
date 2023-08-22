848464a09cec1797b2513f14b07ab0c124934bb8
Cédric <seedy.dupuis@gmail.com>
2023-08-22 15:56:53 +0200

    chore(core): add highlight, configure env, zod validation

---
+++ env.mjs
@@ -0,0 +1,17 @@
+import { createEnv } from "@t3-oss/env-nextjs"
+import { z } from "zod"
+
+export const env = createEnv({
+    server: {
+        MAILER_PORT: z.string().min(1),
+        MAILER_HOST: z.string().url(),
+        MAILER_USER: z.string().email(),
+        MAILER_PASSWORD: z.string().min(1),
+    },
+    client: {
+        NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID: z.string().min(1)
+    },
+    runtimeEnv: {
+        NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID: process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID
+    }
+})
