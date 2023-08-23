970eae7aa10768446146060b4ac9d1b8d7d4e019
Cédric <seedy.dupuis@gmail.com>
2023-08-23 21:55:25 +0200

    feat(typography): frank ruhl libre

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,22 +5,27 @@ import trpc from "helpers/trpc";
 import ToastProvider from "components/Toast/Provider";
 import { HighlightInit } from "@highlight-run/next/client";
 import { env } from "env.mjs"
+import { Frank_Ruhl_Libre } from "next/font/google"
+
+const frankRuhlLibre = Frank_Ruhl_Libre({ subsets: ['latin'] })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfigProvider>
-      <ToastProvider>
-        <HighlightInit
-          projectId={env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID}
-          tracingOrigins
-          networkRecording={{
-            enabled: true,
-            recordHeadersAndBody: true
-          }}
-        />
-        <Component {...pageProps} />
-      </ToastProvider>
-    </SWRConfigProvider>
+    <div className={frankRuhlLibre.className}>
+      <SWRConfigProvider>
+        <ToastProvider>
+          <HighlightInit
+            projectId={env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID}
+            tracingOrigins
+            networkRecording={{
+              enabled: true,
+              recordHeadersAndBody: true
+            }}
+          />
+          <Component {...pageProps} />
+        </ToastProvider>
+      </SWRConfigProvider>
+    </div>
   );
 }
 
