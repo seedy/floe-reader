baebfc8329228ff04b44751f374ee36b154694ed
Cédric <seedy.dupuis@gmail.com>
2023-09-06 23:55:56 +0200

    feat(Button): update according to DS

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,15 +1,17 @@
 import { ComponentProps, ElementRef, forwardRef, ReactNode } from "react";
-import styles from "components/Button/Button.module.css";
+import styles from "components/Button/Button.module.scss";
 import variantsToClassNameStyles from "helpers/variantsToClassNameStyles";
 import classNames from "helpers/classNames";
 
 interface ButtonProps extends ComponentProps<"button"> {
   children?: ReactNode;
   variant?: "contained" | "outlined" | "text";
+  color?: 'primary' | "secondary"
+  mobile?: boolean
 }
 const Button = forwardRef<ElementRef<"button">, ButtonProps>(
-  ({ children, variant = "outlined", className, ...props }, forwardedRef) => {
-    const variantsClassName = variantsToClassNameStyles({ variant }, styles);
+  ({ children, variant = "outlined", color = "primary", mobile, className, ...props }, forwardedRef) => {
+    const variantsClassName = variantsToClassNameStyles({ variant, color, mobile }, styles);
     return (
       <button
         ref={forwardedRef}
