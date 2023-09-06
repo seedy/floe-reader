62af4afaf7abf1244851ce64fb18d887f8f5494c
Cédric <seedy.dupuis@gmail.com>
2023-09-06 22:16:42 +0200

    feat(Video): video skeleton

---
+++ components/Video/index.tsx
@@ -0,0 +1,23 @@
+import styles from "components/Video/Video.module.css";
+import { UpdateIcon } from "@radix-ui/react-icons";
+import Box from "components/Box";
+
+interface VideoProps {
+    src: string
+    title: string
+}
+const Video = ({ src, title }: VideoProps) => {
+
+    return (
+        <>
+            <div className={styles.skeleton}>
+                <UpdateIcon className={styles.skeletonIcon} />
+            </div>
+            <iframe
+                className={styles.player}
+                src={src} frameBorder="0" title={title} />
+        </>
+    )
+}
+
+export default Video
