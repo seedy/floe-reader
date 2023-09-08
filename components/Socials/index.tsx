56e5487e2feec0fdc1c98a2ebd52a4a28975a9c0
Cédric <seedy.dupuis@gmail.com>
2023-09-08 14:55:14 +0200

    feat(Links): rework links and socials

--- components/Socials/index.tsx
+++ components/Socials/index.tsx
@@ -1,19 +1,19 @@
 import { InstagramLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 import Flex from "components/Flex"
-import IconLink from "components/IconLink";
+import IconButtonLink from "components/IconButton/Link";
 import styles from "components/Socials/Socials.module.css";
 
 const Socials = () => (
     <Flex className={styles.root} align="center">
-        <IconLink
+        <IconButtonLink
             href="https://www.instagram.com/c_ben_correc/"
             target="_blank"
             variant="small">
             <InstagramLogoIcon />
-        </IconLink>
-        <IconLink href="https://www.linkedin.com/in/flo%C3%A9-gaubert-cbencorrec/" target="_blank" variant="small">
+        </IconButtonLink>
+        <IconButtonLink href="https://www.linkedin.com/in/flo%C3%A9-gaubert-cbencorrec/" target="_blank" variant="small">
             <LinkedInLogoIcon />
-        </IconLink>
+        </IconButtonLink>
     </Flex>
 )
 
