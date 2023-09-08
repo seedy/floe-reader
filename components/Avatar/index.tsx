f3e43e9cdd6c78c38a9f49c3da71babd244d35bf
Cédric <seedy.dupuis@gmail.com>
2023-09-08 16:50:28 +0200

    feat(Avatar): redesign

--- components/Avatar/index.tsx
+++ components/Avatar/index.tsx
@@ -1,24 +1,28 @@
 import Image from "next/image";
 import { ComponentProps } from "react";
-import styles from "components/Avatar/Avatar.module.css";
+import styles from "components/Avatar/Avatar.module.scss";
 import classNames from "helpers/classNames";
-
-// CONSTANTS
-export const SIZE = 160;
+import variantsToClassNameStyles from "helpers/variantsToClassNameStyles";
 
 // COMPONENTS
-export interface AvatarProps extends ComponentProps<typeof Image> { }
+export interface AvatarProps extends ComponentProps<typeof Image> {
+  selected?: boolean
+}
 
-const Avatar = ({ src, alt, className, ...props }: AvatarProps) => (
-  <Image
-    className={classNames(className, styles.root)}
-    width={SIZE}
-    height={SIZE}
-    src={src || `https://picsum.photos/${SIZE}`}
-    alt={alt}
-    placeholder="blur"
-    {...props}
-  />
-);
+const Avatar = ({ src, alt, className, selected, ...props }: AvatarProps) => {
+  const variantsClassName = variantsToClassNameStyles(
+    { selected },
+    styles
+  );
+  return (
+    <Image
+      className={classNames(styles.root, className, variantsClassName)}
+      src={src}
+      alt={alt}
+      placeholder="blur"
+      {...props}
+    />
+  );
+}
 
 export default Avatar;
