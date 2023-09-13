cf4760277c5790d1ab181977d3a65bdc7f8ece7c
Cédric <seedy.dupuis@gmail.com>
2023-09-13 23:14:58 +0200

    feat(Video): preload and loading props

--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -1,20 +1,29 @@
 import styles from "components/Video/Video.module.css";
 import { UpdateIcon } from "@radix-ui/react-icons";
+import { ComponentProps } from "react";
+import Head from "next/head";
 
-interface VideoProps {
+interface VideoProps extends ComponentProps<"iframe"> {
     src: string
     title: string
+    preload?: boolean
 }
-const Video = ({ src, title }: VideoProps) => {
+const Video = ({ src, title, loading, preload, ...props }: VideoProps) => {
 
     return (
         <>
+            {preload && (
+                <Head>
+                    <link href={src} rel="preload" as="script" />
+                </Head>
+            )}
             <div className={styles.skeleton}>
                 <UpdateIcon className={styles.skeletonIcon} />
             </div>
             <iframe
+                loading={loading}
                 className={styles.player}
-                src={src} frameBorder="0" title={title} />
+                src={src} frameBorder="0" title={title} {...props} />
         </>
     )
 }
