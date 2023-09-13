70adace7e5aa8a41cfae8734530982bccfaa001d
Cédric <seedy.dupuis@gmail.com>
2023-09-14 00:15:43 +0200

    fix(Video): no preload

--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -1,27 +1,18 @@
 import styles from "components/Video/Video.module.css";
 import { UpdateIcon } from "@radix-ui/react-icons";
 import { ComponentProps } from "react";
-import Head from "next/head";
 
 interface VideoProps extends ComponentProps<"iframe"> {
     src: string
     title: string
-    preload?: boolean
 }
-const Video = ({ src, title, loading, preload, ...props }: VideoProps) => {
-
+const Video = ({ src, title, ...props }: VideoProps) => {
     return (
         <>
-            {preload && (
-                <Head>
-                    <link href={src} rel="preload" as="script" />
-                </Head>
-            )}
             <div className={styles.skeleton}>
                 <UpdateIcon className={styles.skeletonIcon} />
             </div>
             <iframe
-                loading={loading}
                 className={styles.player}
                 src={src} frameBorder="0" title={title} {...props} />
         </>
