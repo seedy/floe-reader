717aec9fd54461ae891d4042e6c5000535953f5f
Cédric <seedy.dupuis@gmail.com>
2023-09-16 01:08:22 +0200

    feat(Landing): improve animations, video layout

--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -1,20 +1,24 @@
 import styles from "components/Video/Video.module.css";
+import classNames from "helpers/classNames";
 import { UpdateIcon } from "@radix-ui/react-icons";
 import { ComponentProps } from "react";
 
 interface VideoProps extends ComponentProps<"iframe"> {
     src: string
     title: string
+    clipperClassName?: string
 }
-const Video = ({ src, title, ...props }: VideoProps) => {
+const Video = ({ src, title, className, clipperClassName, ...props }: VideoProps) => {
     return (
         <>
             <div className={styles.skeleton}>
                 <UpdateIcon className={styles.skeletonIcon} />
             </div>
-            <iframe
-                className={styles.player}
-                src={src} frameBorder="0" title={title} {...props} />
+            <div className={classNames(styles.clipper, clipperClassName)}>
+                <iframe
+                    className={classNames(styles.player, className)}
+                    src={src} frameBorder="0" title={title} {...props} />
+            </div>
         </>
     )
 }
