aa0ccfe1b476e5f8db17fa0cf18c4963b45bd68f
Cédric <seedy.dupuis@gmail.com>
2023-09-16 02:27:13 +0200

    feat(desktop): wip

--- components/Socials/index.tsx
+++ components/Socials/index.tsx
@@ -2,9 +2,13 @@ import { InstagramLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 import Flex from "components/Flex"
 import IconButtonLink from "components/IconButton/Link";
 import styles from "components/Socials/Socials.module.css";
+import classNames from "helpers/classNames";
+import { ComponentProps } from "react";
 
-const Socials = () => (
-    <Flex className={styles.root} align="center">
+interface SocialsProps extends ComponentProps<typeof Flex> {
+}
+const Socials = ({ className }: SocialsProps) => (
+    <Flex className={classNames(styles.root, className)} align="center">
         <IconButtonLink
             href="https://www.instagram.com/c_ben_correc/"
             target="_blank"
