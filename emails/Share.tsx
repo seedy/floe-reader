07e450eeda28459f6ff1d972163633cb164b608f
Cédric <seedy.dupuis@gmail.com>
2023-12-06 19:43:16 +0100

    wip: email

--- emails/Share.tsx
+++ emails/Share.tsx
@@ -14,14 +14,17 @@ import getBaseUrl from "../helpers/getBaseUrl"
 const Share = () => (
     <Html>
         <Head />
-        <Preview>Suite à notre rencontre, ci-joint ma carte de visite</Preview>
+        <Preview>[Floé Gaubert] Suite à notre rencontre</Preview>
         <Body style={{ backgroundColor: "hsl(225, 100%, 99%)", color: "hsl(0, 0%, 21%)" }}>
-            <Container style={{ padding: '1.5rem' }}>
-                <Img src={`${getBaseUrl()}/public/CBC_LOGO_FULL.png`} height="512" width="492" alt="CBenCorrec'" />
-                <Heading style={{ fontSize: "1.75rem", fontWeight: 'bold' }}>Suite à notre rencontre, ci-joint ma carte de visite</Heading>
+            <Container style={{ padding: '0 1.5rem 1.5rem', overflowX: "hidden", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+                <Section style={{ position: "relative", width: "fit-content" }}>
+                    <Img style={{ margin: 'auto' }} height={360} src="/static/landing-1-mask.png" alt="Réalisons les contenus qui feront parler de toi'" />
+                    <Heading style={{ fontSize: "1.75rem", fontWeight: 'bold', position: 'absolute', top: 0, left: 0, right: 0, textAlign: "center", color: "white" }}>Réalisons les contenus qui feront parler de toi</Heading>
+                </Section>
 
                 <Section style={{ padding: "1.5rem", margin: "0 2.5rem" }}>
                     <Heading as="h2">Floé Gaubert</Heading>
+                    <Heading as="h2">Conceptrice rédactrice</Heading>
                     <Heading as="h2">Tel: <Link href="tel:0683988242">06 83 98 82 42</Link></Heading>
                     <Heading as="h2">Mail : <Link href="mailto:cbencorrec@gmail.com">cbencorrec@gmail.com</Link></Heading>
                     <Link href={getBaseUrl()}>Visiter mon site vitrine</Link>
