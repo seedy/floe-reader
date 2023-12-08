7c509bf3643348371360e2b100d0f94f5b223b43
Cédric <seedy.dupuis@gmail.com>
2023-12-08 16:45:43 +0100

    feat(Share): email template fix

--- helpers/getBaseUrl/index.ts
+++ helpers/getBaseUrl/index.ts
@@ -1,14 +1,16 @@
 const getBaseUrl = () => {
-    if (typeof window !== 'undefined')
-        // browser origin
-        return window.location.origin;
+	if (typeof window !== "undefined") {
+		// browser origin
+		return window.location.origin;
+	}
 
-    if (process.env.VERCEL_URL)
-        // reference for vercel.com
-        return `https://${process.env.VERCEL_URL}`;
+	if (process.env.VERCEL_URL) {
+		// reference for vercel.com
+		return `https://${process.env.VERCEL_URL}`;
+	}
 
-    // assume localhost
-    return `http://localhost:${process.env.PORT ?? 3000}`;
-}
+	// assume localhost
+	return `http://localhost:${process.env.PORT ?? 3000}`;
+};
 
-export default getBaseUrl
+export default getBaseUrl;
