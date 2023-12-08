19368ef2ca8a94bcaf5fcd34535806c2000c2957
Cédric <seedy.dupuis@gmail.com>
2023-12-08 17:52:33 +0100

    feat(email): send email from app

--- components/Share/Dropdown/index.tsx
+++ components/Share/Dropdown/index.tsx
@@ -1,55 +1,70 @@
-import { CopyIcon, EnvelopeClosedIcon, MixIcon } from "@radix-ui/react-icons"
-import { Root, Trigger, Content, Label, Item, Group } from "@radix-ui/react-dropdown-menu"
-import styles from "components/Share/Share.module.css"
-import Button from "components/Button"
-import { ReactNode } from "react"
-import H2 from "components/Typography/H2"
-import Flex from "components/Flex"
+import { CopyIcon, EnvelopeClosedIcon, MixIcon } from "@radix-ui/react-icons";
+import {
+	Root,
+	Trigger,
+	Content,
+	Label,
+	Item,
+	Group,
+} from "@radix-ui/react-dropdown-menu";
+import styles from "components/Share/Share.module.css";
+import Button from "components/Button";
+import { ReactNode } from "react";
+import H2 from "components/Typography/H2";
+import Flex from "components/Flex";
 
 interface ShareDropdownProps {
-    open: boolean
-    onOpenChange: (value: boolean) => void
-    onQrCodeOpen: (e: Event) => void
-    onShareMail: () => void
-    onCopyLink: () => void
-    trigger: ReactNode
-    children: ReactNode
+	open: boolean;
+	onOpenChange: (value: boolean) => void;
+	onQrCodeOpen: (e: Event) => void;
+	onShareMail: (e: Event) => void;
+	onCopyLink: () => void;
+	trigger: ReactNode;
+	children: ReactNode;
 }
-const ShareDropdown = ({ open, onOpenChange, onQrCodeOpen, onShareMail, onCopyLink, trigger, children }: ShareDropdownProps) => (
-    <Root open={open} onOpenChange={onOpenChange}>
-        <Trigger asChild>
-            {trigger}
-        </Trigger>
-        <Content className={styles.content} collisionPadding={{ top: 20, right: 20 }}>
-            <Label asChild>
-                <H2 className={styles.label}>Partager mon site vitrine</H2>
-            </Label>
-            <Group asChild>
-                <Flex align="start" direction="column">
-                    <Item asChild onSelect={onQrCodeOpen}>
-                        <Button className={styles.item} type="button" variant="link">
-                            <MixIcon />
-                            QR code
-                        </Button>
-                    </Item>
-                    <Item asChild onSelect={onShareMail}>
-                        <Button className={styles.item} type="button" variant="link">
-                            <EnvelopeClosedIcon />
-                            Mail automatique
-                        </Button>
-                    </Item>
-                    <Item asChild onSelect={onCopyLink}>
-                        <Button className={styles.item} type="button" variant="link">
-                            <CopyIcon />
-                            Copier le lien
-                        </Button>
-                    </Item>
+const ShareDropdown = ({
+	open,
+	onOpenChange,
+	onQrCodeOpen,
+	onShareMail,
+	onCopyLink,
+	trigger,
+	children,
+}: ShareDropdownProps) => (
+	<Root open={open} onOpenChange={onOpenChange}>
+		<Trigger asChild>{trigger}</Trigger>
+		<Content
+			className={styles.content}
+			collisionPadding={{ top: 20, right: 20 }}
+		>
+			<Label asChild>
+				<H2 className={styles.label}>Partager mon site vitrine</H2>
+			</Label>
+			<Group asChild>
+				<Flex align="start" direction="column">
+					<Item asChild onSelect={onQrCodeOpen}>
+						<Button className={styles.item} type="button" variant="link">
+							<MixIcon />
+							QR code
+						</Button>
+					</Item>
+					<Item asChild onSelect={onShareMail}>
+						<Button className={styles.item} type="button" variant="link">
+							<EnvelopeClosedIcon />
+							Mail automatique
+						</Button>
+					</Item>
+					<Item asChild onSelect={onCopyLink}>
+						<Button className={styles.item} type="button" variant="link">
+							<CopyIcon />
+							Copier le lien
+						</Button>
+					</Item>
+				</Flex>
+			</Group>
+		</Content>
+		{children}
+	</Root>
+);
 
-                </Flex>
-            </Group>
-        </Content>
-        {children}
-    </Root>
-)
-
-export default ShareDropdown
+export default ShareDropdown;
---
+++ components/Share/Email/index.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import {
+	Root,
+	Overlay,
+	Content,
+	Title,
+	Close,
+	Description,
+} from "@radix-ui/react-dialog";
+import { Cross2Icon, EnterIcon } from "@radix-ui/react-icons";
+import { share } from "server/actions";
+import Flex from "components/Flex";
+import IconButton from "components/IconButton";
+import SlotSubmit from "components/Slot/Submit";
+import Input from "components/Input";
+import H2 from "components/Typography/H2";
+import H3 from "components/Typography/H3";
+import styles from "components/Share/Email/Email.module.css";
+import { ReactNode, useEffect, useRef } from "react";
+// @ts-ignore
+import { experimental_useFormState as useFormState } from "react-dom";
+
+interface ShareEmailProps {
+	open: boolean;
+	onOpenChange: (value: boolean) => void;
+	onSent: () => void;
+	onError: () => void;
+}
+const ShareEmail = ({
+	open,
+	onOpenChange,
+	onSent,
+	onError,
+}: ShareEmailProps) => {
+	const [state, formAction] = useFormState(share, undefined);
+	const onSentRef = useRef(onSent);
+	const onErrorRef = useRef(onError);
+
+	useEffect(() => {
+		if (state !== undefined) {
+			return state?.success ? onSentRef.current() : onErrorRef.current();
+		}
+	}, [state]);
+
+	return (
+		<Root open={open} onOpenChange={onOpenChange}>
+			<Overlay />
+			<Content className={styles.content}>
+				<Title asChild>
+					<H2 className={styles.label}>Partager la carte de visite</H2>
+				</Title>
+				<Description asChild>
+					<H3 className={styles.description}>
+						Veuillez indiquer le mail du destinataire
+					</H3>
+				</Description>
+				<form action={formAction}>
+					<Flex className={styles.formRow}>
+						<Input label="Mail du destinataire" name="email" />
+						<SlotSubmit>
+							<IconButton variant="small" type="submit">
+								<EnterIcon />
+							</IconButton>
+						</SlotSubmit>
+					</Flex>
+				</form>
+				<Close asChild aria-label="Fermer">
+					<IconButton className={styles.close} variant="small" type="button">
+						<Cross2Icon />
+					</IconButton>
+				</Close>
+			</Content>
+		</Root>
+	);
+};
+
+export default ShareEmail;
