306577756b1ee11bcb1a075234ccba28998316b3
Cédric <seedy.dupuis@gmail.com>
2023-12-09 16:04:19 +0100

    chore(experimental): cleanup previously experimental features

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,21 @@
-import "./env.mjs"
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
+import "./env.mjs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import { withHighlightConfig } from "@highlight-run/next/config";
-import nextBuildId from "next-build-id"
+import nextBuildId from "next-build-id";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  generateBuildId: () => nextBuildId({ dir: __dirname }),
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    instrumentationHook: true,
-    serverActions: true,
-  },
-  productionBrowserSourceMaps: true,
+	generateBuildId: () => nextBuildId({ dir: __dirname }),
+	reactStrictMode: true,
+	swcMinify: true,
+	experimental: {
+		instrumentationHook: true,
+	},
+	productionBrowserSourceMaps: true,
 };
 
 export default withHighlightConfig(nextConfig);
