ebfbd1e17c6c55b4a7f7e97198e69dad7eac11d4
Cédric <seedy.dupuis@gmail.com>
2024-03-29 15:34:30 +0100

    chore: integrate tailwind, cva

---
+++ helpers/cn/index.ts
@@ -0,0 +1,6 @@
+import type { CxOptions } from "class-variance-authority";
+import { cx } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+
+const cn = (...inputs: CxOptions) => twMerge(cx(inputs));
+export default cn;
