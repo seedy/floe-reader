b6fe01fb003007c9cfd06fed3919d4bda2de21b4
Cédric <seedy.dupuis@gmail.com>
2024-04-15 17:12:45 +0200

    feat(ScreenHero): wip

--- components/Screen/Hero/index.tsx
+++ components/Screen/Hero/index.tsx
@@ -4,30 +4,50 @@ import Screen from "components/Screen";
 import H2 from "components/Typography/H2";
 import H3 from "components/Typography/H3";
 import P from "components/Typography/P";
+import SpanUnderline from "components/Typography/SpanUnderline";
 import { CALENDLY } from "constants/links";
+import cn from "helpers/cn";
 import eidLabLogo from "public/eidlab-logo.webp";
 import kinobaLogo from "public/kinoba-logo.svg";
 import savoieProcessLogo from "public/savoie-process-logo.svg";
 
 const ScreenHero = () => (
 	<Screen className="flex h-svh flex-col">
-		<div className="mx-5 mb-5 mt-6 flex grow flex-col justify-between">
-			<div className="flex flex-col gap-10">
-				<H2>Et si on commençait par se rencontrer ?</H2>
-				<P>
-					Toutes les grandes histoires commencent par une rencontre. Quelle est
-					la tienne ?
-					<br />
-					Prenons le temps de parler de ton projet !
-				</P>
-				<H3>Ils m&apos;ont confié leur communication</H3>
-				<div className="flex flex-col gap-5">
-					<div className="items-center gap-4">
-						<Logo height={40} src={eidLabLogo} alt="Eid Lab" />
-						<P className="font-bold">Eid Lab</P>
+		<div
+			className={cn(
+				"mx-5 mb-5 mt-6 flex grow flex-col justify-between",
+				"lg:items-center",
+			)}
+		>
+			<div className={cn("flex flex-col gap-16", "lg:gap-[7.5rem]")}>
+				<div className={cn("flex flex-col gap-10", "lg:gap-20")}>
+					<H2>
+						Et si on commençait par se{" "}
+						<SpanUnderline variant="primary">rencontrer ?</SpanUnderline>
+					</H2>
+					<div>
+						<P>Toutes les grandes histoires commencent par une rencontre.</P>
+						<P>Quelle est la tienne ?</P>
+						<P className="lg:mt-6">
+							Prenons le temps de parler de ton projet !
+						</P>
 					</div>
-					<Logo height={40} src={kinobaLogo} alt="Kinoba" />
-					<Logo height={40} src={savoieProcessLogo} alt="Savoie Process" />
+				</div>
+				<div className={cn("flex flex-col gap-10", "lg:gap-20")}>
+					<H3>Ils m&apos;ont confié leur communication</H3>
+					<div
+						className={cn(
+							"flex flex-col gap-5",
+							"lg:flex-row lg:gap-10 lg:px-5",
+						)}
+					>
+						<div className="flex items-center gap-4">
+							<Logo height={40} src={eidLabLogo} alt="Eid Lab" />
+							<P className="font-bold">Eid Lab</P>
+						</div>
+						<Logo height={40} src={kinobaLogo} alt="Kinoba" />
+						<Logo height={40} src={savoieProcessLogo} alt="Savoie Process" />
+					</div>
 				</div>
 			</div>
 			<div className="flex flex-col items-center gap-5">
