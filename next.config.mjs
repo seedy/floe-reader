b7ac9f939ac1689651812b2ce4497132a5d8cada
Cédric <seedy.dupuis@gmail.com>
2024-05-31 17:50:33 +0200

    chore(devdeps): install million lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,28 @@
+import MillionLint from '@million/lint';
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { withHighlightConfig } from "@highlight-run/next/config";
 import NextBundleAnalyzer from "@next/bundle-analyzer";
 import nextBuildId from "next-build-id";
 import "./env.mjs";
-
 const withBundleAnalyzer = NextBundleAnalyzer({
-	enabled: process.env.ANALYZE === "true",
+  enabled: process.env.ANALYZE === "true"
 });
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	generateBuildId: () => nextBuildId({ dir: __dirname }),
-	reactStrictMode: true,
-	swcMinify: true,
-	experimental: {
-		instrumentationHook: true,
-	},
-	productionBrowserSourceMaps: true,
+  generateBuildId: () => nextBuildId({
+    dir: __dirname
+  }),
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    instrumentationHook: true
+  },
+  productionBrowserSourceMaps: true
 };
-
-export default withBundleAnalyzer(withHighlightConfig(nextConfig));
+export default MillionLint.next({
+  rsc: true
+})(withBundleAnalyzer(withHighlightConfig(nextConfig)));
