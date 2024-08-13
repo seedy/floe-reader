2dd418e8c9d9ec6a518933ea5f453e45e207a0d5
Cédric <seedy.dupuis@gmail.com>
2024-08-13 11:55:58 +0200

    feat(ButtonLink): external

--- components/Button/Link/index.tsx
+++ components/Button/Link/index.tsx
@@ -1,3 +1,4 @@
+import { ExternalLinkIcon } from "@radix-ui/react-icons";
 import { buttonVariants } from "components/Button/variants";
 import cn from "helpers/cn";
 import { ComponentProps, ElementRef, ReactNode, forwardRef } from "react";
@@ -6,10 +7,18 @@ interface ButtonLinkProps extends ComponentProps<"a"> {
 	children?: ReactNode;
 	variant?: "contained" | "outlined" | "link";
 	color?: "primary" | "secondary";
+	external?: boolean;
 }
 const ButtonLink = forwardRef<ElementRef<"a">, ButtonLinkProps>(
 	(
-		{ children, variant = "link", color = "primary", className, ...props },
+		{
+			children,
+			variant = "link",
+			color = "primary",
+			className,
+			external,
+			...props
+		},
 		forwardedRef,
 	) => {
 		const buttonVariantsClassName = buttonVariants({ variant, color });
@@ -21,6 +30,7 @@ const ButtonLink = forwardRef<ElementRef<"a">, ButtonLinkProps>(
 				{...props}
 			>
 				{children}
+				{external && <ExternalLinkIcon />}
 			</a>
 		);
 	},
