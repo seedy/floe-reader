b8cf92a2ad4f076e59bbbc9d79142f6680e1c4f1
Seedy <seedy.dupuis@gmail.com>
2024-08-26 12:33:19 +0200

    102 footer (#113)

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,14 @@
-import Screen from "components/Screen";
+import Footer from "components/Footer";
 import ScreenHero from "components/Screen/Hero";
 import ScreenLanding from "components/Screen/Landing";
 import ScreenPresentation from "components/Screen/Presentation";
-import { LOREM } from "constants/lorem";
 
 const Home = () => (
 	<>
 		<ScreenLanding />
 		<ScreenPresentation />
 		<ScreenHero />
-		<Screen>
-			<div>{LOREM}</div>
-			<div>{LOREM}</div>
-			<div>{LOREM}</div>
-			<div>{LOREM}</div>
-		</Screen>
+		<Footer />
 	</>
 );
 
---
+++ components/Footer/Footer.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, StoryFn } from "@storybook/react";
+import React from "react";
+
+import Footer from ".";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+	title: "Components/Footer",
+	component: Footer,
+	// More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+} as Meta<typeof Footer>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: StoryFn<typeof Footer> = () => <Footer />;
+
+export const Default = Template.bind({});
