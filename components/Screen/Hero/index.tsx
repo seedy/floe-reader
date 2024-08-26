aabf2af839729b877831991b3ffc8f036dac91b8
Cédric <seedy.dupuis@gmail.com>
2024-08-26 18:55:20 +0200

    wip(Hero): wip

--- components/Screen/Hero/index.tsx
+++ components/Screen/Hero/index.tsx
@@ -1,22 +1,47 @@
+import Avatar from "components/Avatar";
 import ButtonLink from "components/Button/Link";
-import Logo from "components/Logo";
 import Screen from "components/Screen";
 import TabAndTitle from "components/Screen/Hero/TabAndTitle";
 import H2 from "components/Typography/H2";
 import H3 from "components/Typography/H3";
 import P from "components/Typography/P";
-import SpanUnderline from "components/Typography/SpanUnderline";
 import { CALENDLY, SERVICES } from "constants/links";
 import cn from "helpers/cn";
-import eidLabLogo from "public/eidlab-logo.webp";
-import kinobaLogo from "public/kinoba-logo.svg";
-import savoieProcessLogo from "public/savoie-process-logo.svg";
 
 const ScreenHero = () => {
 	return (
 		<>
-			<Screen className="flex h-svh flex-col">
-				<div
+			<Screen
+				className={cn(
+					"flex flex-col bg-primary",
+					"pt-6 lg:pt-32",
+					"px-5 lg:px-0",
+					"gap-10 lg:gap-32",
+				)}
+			>
+				<div className={cn("flex flex-col gap-16")}>
+					<H2 variant="h1" color="white">
+						Et si on commençait par se rencontrer ?
+					</H2>
+					<div>
+						<P color="white">
+							Toutes les grandes histoires commencent par une rencontre.
+						</P>
+						<P color="white">Quelle est la tienne ?</P>
+						<P color="white">Prenons le temps de parler de ton projet !</P>
+					</div>
+				</div>
+				<Avatar src={""} alt="" className="size-80" />
+				<div className="flex flex-col gap-8">
+					<ButtonLink variant="contained" color="secondary" href={CALENDLY}>
+						Je prends rendez-vous
+					</ButtonLink>
+					<P className="text-whiteHint">Mon calendrier t&apos;est ouvert !</P>
+				</div>
+				<div className={cn("")}>
+					<H3>Ils m&apos;ont confié leur communication</H3>
+				</div>
+				{/* <div
 					className={cn(
 						"mx-5 mb-5 mt-6 flex grow flex-col gap-20",
 						"lg:items-center",
@@ -24,10 +49,7 @@ const ScreenHero = () => {
 				>
 					<div className={cn("flex flex-col gap-16", "lg:gap-[7.5rem]")}>
 						<div className={cn("flex flex-col gap-10", "lg:gap-20")}>
-							<H2>
-								Et si on commençait par se{" "}
-								<SpanUnderline variant="primary">rencontrer ?</SpanUnderline>
-							</H2>
+							<H2 variant="h1">Et si on commençait par se rencontrer ?</H2>
 							<div>
 								<P>
 									Toutes les grandes histoires commencent par une rencontre.
@@ -68,6 +90,7 @@ const ScreenHero = () => {
 						<P className="text-textHint">Mon calendrier t&apos;est ouvert !</P>
 					</div>
 				</div>
+				*/}
 			</Screen>
 			<Screen id={SERVICES.replace("#", "")}>
 				<TabAndTitle />
