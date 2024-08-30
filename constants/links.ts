2a2d845612e32019a1c0d55f6f1ec2822c23fc9a
Cédric <seedy.dupuis@gmail.com>
2024-08-30 10:01:55 +0200

    chore(links): update branded links

--- constants/links.ts
+++ constants/links.ts
@@ -3,12 +3,11 @@ export const ZCAL_INTERVIEW = "https://zcal.co/floe-gaubert/interview";
 export const ZCAL_MONTAGE = "https://zcal.co/floe-gaubert/montage";
 export const ZCAL_PHOTO = "https://zcal.co/floe-gaubert/photo";
 
-export const INSTAGRAM = "https://www.instagram.com/c_ben_correc/";
-export const LINKEDIN =
-	"https://www.linkedin.com/in/flo%C3%A9-gaubert-cbencorrec/";
+export const INSTAGRAM = "https://www.instagram.com/floe.gaubert/";
+export const LINKEDIN = "https://www.linkedin.com/in/floé-gaubert/";
 export const PORTFOLIO = "https://cbencorrec.myportfolio.com/";
 export const YOUTUBE =
-	"https://www.youtube.com/@Flo%C3%A9Gaubert-Monteusevideo?sub_confirmation=1";
-export const EMAIL = "mailto:cbencorrec@gmail.com";
+	"https://www.youtube.com/@FloéGaubert-Monteusevideo?sub_confirmation=1";
+export const EMAIL = "mailto:floe.gaubert+pro@gmail.com";
 export const TEL = "tel:0683988242";
 export const SERVICES = "#services";
