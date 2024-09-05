30290397be941df34c6905e9568b3046ca45a1a3
Seedy <seedy.dupuis@gmail.com>
2024-09-05 18:10:29 +0200

    112 sitemap (#152)

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,10 +3,22 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
-			url: "https://acme.com",
+			url: "https://floegaubert.com",
+			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 1,
+		},
+		{
+			url: "https://floegaubert.com/legal",
 			lastModified: new Date(),
 			changeFrequency: "yearly",
-			priority: 1,
+			priority: 0.5,
+		},
+		{
+			url: "https://floegaubert.com/privacy",
+			lastModified: new Date(),
+			changeFrequency: "yearly",
+			priority: 0.5,
 		},
 	];
 }
