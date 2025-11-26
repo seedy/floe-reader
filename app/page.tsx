121cc59123c621ae6e05f59bdb0505deb1a88c32
seedy <seedy.dupuis@gmail.com>
2025-11-26 01:34:38 +0100

    fix: remove test

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import ScreenHero from "components/Screen/Hero";
 import ScreenLanding from "components/Screen/Landing";
 import ScreenPresentation from "components/Screen/Presentation";
-import SocialsEmailOrApp from "components/Socials/EmailOrApp";
 
 const Home = () => {
   return (
     <div className="flex flex-col gap-32">
-      <SocialsEmailOrApp />
       <ScreenLanding />
       <ScreenPresentation>
         <ScreenHero />
